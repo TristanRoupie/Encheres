64ba16500806d7d83dc98d34d0316dfe6bc2e39d
asylvain2019 <asylvain2019@44-UC45-13.ad.campus-eni.fr>
2019-04-29 11:41:19 +0200

    add class Utilisateur to BO

---
+++ src/fr/eni/encheres/bo/Utilisateur.java
@@ -0,0 +1,16 @@
+package fr.eni.encheres.bo;
+
+public class Utilisateur {
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private String motDePasse;
+	private int credit;
+	private String administrateur;
+}
