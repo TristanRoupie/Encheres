33003f8abbf7ee85801e51a9cd155bb64594a316
marabou <marabou@marabouDELL>
2019-04-29 23:11:36 +0200

    -modification de la classe Utilisateur

--- src/fr/eni/encheres/bo/Utilisateur.java
+++ src/fr/eni/encheres/bo/Utilisateur.java
@@ -1,37 +1,62 @@
 package fr.eni.encheres.bo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
+//credit INTEGER NOT NULL,administrateur bit NOT NULL)
+
+@Entity
+@Table(name = "UTILISATEUR")
 @Data
 public class Utilisateur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "no_utilisateur", length=11, nullable=false, unique=true)
 	private int noUtilisateur;
+	@Column(name = "pseudo", length=30, nullable=false)
 	private String pseudo;
+	@Column(name = "nom", length=30, nullable=false)
 	private String nom;
+	@Column(name = "prenom", length=30, nullable=false)
 	private String prenom;
+	@Column(name = "email", length=20, nullable=false)
 	private String email;
+	@Column(name = "email", length=15)
 	private String telephone;
+	@Column(name = "rue", length=30, nullable=false)
 	private String rue;
+	@Column(name = "code_postal", length=10, nullable=false)
 	private int codePostal;
+	@Column(name = "ville", length=30, nullable=false)
 	private String ville;
+	@Column(name = "mot_de_passe", length=30, nullable=false)
 	private String motDePasse;
+	@Column(name = "credit", length=11, nullable=false, columnDefinition = "int default 0")
 	private int credit;
+	@Column(name = "administrateur", length=1, nullable=false, columnDefinition = "int default 0")
 	private byte administrateur; // 0 or 1 because Lombok bugs with boolean
 
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, int codePostal, String ville, String motDePasse, int credit, byte administrateur) {
 		super();
-		this.noUtilisateur = noUtilisateur;
-		this.pseudo = pseudo;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.email = email;
-		this.telephone = telephone;
-		this.rue = rue;
-		this.codePostal = codePostal;
-		this.ville = ville;
-		this.motDePasse = motDePasse;
-		this.credit = credit;
-		this.administrateur = administrateur; 
+		this.setNoUtilisateur(noUtilisateur);
+		this.setPseudo (pseudo);
+		this.setNom (nom);
+		this.setPrenom (prenom);
+		this.setEmail (email);
+		this.setTelephone (telephone);
+		this.setRue (rue);
+		this.setCodePostal(codePostal);
+		this.setVille (ville);
+		this.setMotDePasse (motDePasse);
+		this.setCredit (credit);
+		this.setAdministrateur (administrateur);
 	}
 
 	public Utilisateur() {
