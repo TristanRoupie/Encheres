0c68f3a54af8ca1fb997ef3d8b32219b8ce28703
TristanRoupie <tristan.roupie2019@campus-eni.fr>
2019-04-30 16:59:56 +0200

    -add categorie

--- src/fr/eni/encheres/bo/Categorie.java
+++ src/fr/eni/encheres/bo/Categorie.java
@@ -1,12 +1,21 @@
 package fr.eni.encheres.bo;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
 public class Categorie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "no_Categorie", length=11, nullable=false, unique=true)
 	private int noCategorie;
+	@Column(name = "libelle", length=30, nullable=false)
 	private String libelle;
-
+			
 	public Categorie(int noCategorie, String libelle) {
 		super();
 		this.noCategorie = noCategorie;
--- src/fr/eni/encheres/dal/ArticleVenduDAO.java
+++ src/fr/eni/encheres/dal/ArticleVenduDAO.java
@@ -1,10 +1,16 @@
 package fr.eni.encheres.dal;
 
+import java.util.List;
+
 import fr.eni.encheres.bo.ArticleVendu;
+
 
 public interface ArticleVenduDAO {
 	
-	public void addArticle(ArticleVendu article);
+	public void addArticle(ArticleVendu artilce);
+	public void updateArticle(ArticleVendu artilce);
+	public void deleteArticle(ArticleVendu artilce);
+	public List<ArticleVendu> selectAllArticle();
 	
 	
 
