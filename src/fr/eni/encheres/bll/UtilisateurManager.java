21cbd9a6da83fc8d1c5dd3974bd8af3b4bab984b
Aimeric <aimericsylvain@yahoo.fr>
2019-05-02 13:46:15 +0200

    refacto : delete a useless import in UtilisateurManager

--- src/fr/eni/encheres/bll/UtilisateurManager.java
+++ src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -3,8 +3,6 @@ package fr.eni.encheres.bll;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.sun.xml.bind.v2.TODO;
-
 import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.UtilisateurDAO;
