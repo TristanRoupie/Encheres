24ead4db2895231b0c3274779ec51a4b0c25564c
Aimeric <aimericsylvain@yahoo.fr>
2019-05-03 14:32:51 +0200

    Add : adds doPost methods in servlet for connection and subscribtion

--- src/fr/eni/encheres/bll/ConnexionInscriptionServlet.java
+++ src/fr/eni/encheres/bll/ConnexionInscriptionServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class CreaCompteServlet
  */
@@ -16,27 +18,65 @@ import javax.servlet.http.HttpServletResponse;
 public class ConnexionInscriptionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String PATH_TO_CONNEXIONINSCRIPTION_JSP = "/WEB-INF/jsp/connexionInscription.jsp";
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnexionInscriptionServlet() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ConnexionInscriptionServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher(PATH_TO_CONNEXIONINSCRIPTION_JSP);
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		Parameters initialisation
+		int noUtilisateur = 0;
+		String pseudo = null;
+		String nom = null;
+		String prenom = null;
+		String email = null;
+		String telephone = null;
+		String rue = null;
+		int codePostal = 0;
+		String ville = null;
+		String motDePasse = null;
+		int credit = 0;
+		int administrateur = 0;
+
+		try {
+//			Parameter input fetch
+			pseudo = request.getParameter("pseudo");
+			nom = request.getParameter("nom");
+			prenom = request.getParameter("prenom");
+			email = request.getParameter("email");
+			telephone = request.getParameter("telephone");
+			rue = request.getParameter("rue");
+			codePostal = Integer.parseInt(request.getParameter("codepostal"));
+			ville = request.getParameter("ville");
+			motDePasse = request.getParameter("mdpnow");
+
+			UtilisateurManager utilisateurManager = new UtilisateurManager();
+			Utilisateur utilisateur = utilisateurManager.ajouterUtilisateur(noUtilisateur, pseudo, nom, prenom, email,
+					telephone, rue, codePostal, ville, motDePasse);
+			request.setAttribute("utilisateur", utilisateur);
+		} catch (BusinessException e) {
+			request.setAttribute("listeCodesErreur", e.getlisteCodesErrors());
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
+		rd.forward(request, response);
 	}
 
 }
