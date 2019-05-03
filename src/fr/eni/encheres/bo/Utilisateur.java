744817a9c060a6c065c6787f7fb547671c78a23b
Aimeric <aimericsylvain@yahoo.fr>
2019-05-03 14:28:20 +0200

    Add : adds new constructor to fit user input when subscribing

--- src/fr/eni/encheres/bo/Utilisateur.java
+++ src/fr/eni/encheres/bo/Utilisateur.java
@@ -20,64 +20,76 @@ import lombok.Data;
 public class Utilisateur {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "no_utilisateur", length=11, nullable=false, unique=true)
+	@Column(name = "no_utilisateur", length = 11, nullable = false, unique = true)
 	private int noUtilisateur;
-	@Column(name = "pseudo", length=30, nullable=false, unique=true)
+	@Column(name = "pseudo", length = 30, nullable = false, unique = true)
 	private String pseudo;
-	@Column(name = "nom", length=30, nullable=false)
+	@Column(name = "nom", length = 30, nullable = false)
 	private String nom;
-	@Column(name = "prenom", length=30, nullable=false)
+	@Column(name = "prenom", length = 30, nullable = false)
 	private String prenom;
-	@Column(name = "email", length=50, nullable=false, unique=true)
+	@Column(name = "email", length = 50, nullable = false, unique = true)
 	private String email;
-	@Column(name = "telephone", length=15, nullable=true)
+	@Column(name = "telephone", length = 15, nullable = true)
 	private String telephone;
-	@Column(name = "rue", length=30, nullable=false)
+	@Column(name = "rue", length = 30, nullable = false)
 	private String rue;
-	@Column(name = "code_postal", length=10, nullable=false)
+	@Column(name = "code_postal", length = 10, nullable = false)
 	private int codePostal;
-	@Column(name = "ville", length=30, nullable=false)
+	@Column(name = "ville", length = 30, nullable = false)
 	private String ville;
-	@Column(name = "mot_de_passe", length=30, nullable=false)
+	@Column(name = "mot_de_passe", length = 30, nullable = false)
 	private String motDePasse;
-	@Column(name = "credit", length=11, nullable=false, columnDefinition = "int default 0")
+	@Column(name = "credit", length = 11, nullable = false, columnDefinition = "int default 0")
 	private int credit;
-	@Column(name = "administrateur", length=1, nullable=false, columnDefinition = "int default 0")
+	@Column(name = "administrateur", length = 1, nullable = false, columnDefinition = "int default 0")
 	private int administrateur; // 0 or 1 because Lombok bugs with boolean
 
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, int codePostal, String ville, String motDePasse, int credit, int administrateur) {
 		super();
 		this.setNoUtilisateur(noUtilisateur);
-		this.setPseudo (pseudo);
-		this.setNom (nom);
-		this.setPrenom (prenom);
-		this.setEmail (email);
-		this.setTelephone (telephone);
-		this.setRue (rue);
+		this.setPseudo(pseudo);
+		this.setNom(nom);
+		this.setPrenom(prenom);
+		this.setEmail(email);
+		this.setTelephone(telephone);
+		this.setRue(rue);
 		this.setCodePostal(codePostal);
-		this.setVille (ville);
-		this.setMotDePasse (motDePasse);
-		this.setCredit (credit);
-		this.setAdministrateur (administrateur);
+		this.setVille(ville);
+		this.setMotDePasse(motDePasse);
+		this.setCredit(credit);
+		this.setAdministrateur(administrateur);
 	}
-	
+
 	public Utilisateur(String pseudo, String nom, String prenom, String email, String rue, int codePostal, String ville,
 			String motDePasse, int credit, int administrateur) {
 		super();
-		this.setPseudo (pseudo);
-		this.setNom (nom);
-		this.setPrenom (prenom);
-		this.setEmail (email);
-		this.setRue (rue);
-		this.setCodePostal (codePostal);
-		this.setVille (ville);
-		this.setMotDePasse ( motDePasse);
-		this.setCredit (credit);
+		this.setPseudo(pseudo);
+		this.setNom(nom);
+		this.setPrenom(prenom);
+		this.setEmail(email);
+		this.setRue(rue);
+		this.setCodePostal(codePostal);
+		this.setVille(ville);
+		this.setMotDePasse(motDePasse);
+		this.setCredit(credit);
 		this.setAdministrateur(administrateur);
 	}
-	
-	
+
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String rue, int codePostal, String ville,
+			String motDePasse) {
+		super();
+		this.setPseudo(pseudo);
+		this.setNom(nom);
+		this.setPrenom(prenom);
+		this.setEmail(email);
+		this.setRue(rue);
+		this.setCodePostal(codePostal);
+		this.setVille(ville);
+		this.setMotDePasse(motDePasse);
+	}
+
 	public Utilisateur() {
 
 	}
