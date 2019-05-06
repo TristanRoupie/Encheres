fc13d626da9a629b6662bfc170b21cc5e5192e53
TristanRoupie <tristan.roupie2019@campus-eni.fr>
2019-05-06 11:33:46 +0200

    -fix connection on Retrait

--- src/fr/eni/encheres/dal/RetraitDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/RetraitDAOJdbcImpl.java
@@ -13,36 +13,38 @@ public class RetraitDAOJdbcImpl implements RetraitDAO {
 
 	@Override
 	public void addRetrait(Retrait retrait) {
-		Session session = ConnectionProvider.session;
+		Session session = ConnectionProvider.getConnection();
 		session.beginTransaction();
 		session.save(retrait);
 		session.getTransaction().commit();
+		session.close();
 
 	}
 
 	@Override
 	public void updateRetrait(Retrait retrait) {
-		Session session = ConnectionProvider.session;
+		Session session = ConnectionProvider.getConnection();
 		session.beginTransaction();
 		session.saveOrUpdate(retrait);
 		session.getTransaction().commit();
-
+		session.close();
 	}
 
 	@Override
 	public void deleteRetrait(Retrait retrait) {
-		Session session = ConnectionProvider.session;
+		Session session = ConnectionProvider.getConnection();
 		session.beginTransaction();
 		session.delete(retrait);
 		session.getTransaction().commit();
-
+		session.close();
 	}
 
 	@Override
 	public List<Retrait> selectAllRetrait() {
-		Session session = ConnectionProvider.session;
+		Session session = ConnectionProvider.getConnection();
 		Query q = session.createQuery("from RETRAITS");
 		List<Retrait> retraits = q.getResultList();
+		session.close();
 		if (retraits.size() == 0) {
 			return null;
 		} else {
@@ -52,8 +54,9 @@ public class RetraitDAOJdbcImpl implements RetraitDAO {
 
 	@Override
 	public Retrait selectRetraitById(int noArticle) {
-		Session session = ConnectionProvider.session;
+		Session session = ConnectionProvider.getConnection();
 		Retrait retrait = (Retrait) session.get(Retrait.class, noArticle);
+		session.close();
 		return retrait;
 
 	}
